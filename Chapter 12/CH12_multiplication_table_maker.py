6e60fd930b14eb7889023b3037c6bfe078f52b7f
Lucas <inpursuitpl@gmail.com>
2017-05-26 11:08:48 +0200

    Update and rename CH12_multiplication_table_maker_before_refactoring.py to Chapter 12/CH12_multiplication_table_maker.py

--- Chapter 12/CH12_multiplication_table_maker.py
+++ Chapter 12/CH12_multiplication_table_maker.py
@@ -7,6 +7,7 @@ if len(sys.argv) < 2:
     print(' I need a number!')
 else:
     arg = sys.argv[1]
+    
 wb = openpyxl.Workbook()
 sheet = wb.active
 for i in range(1,int(arg)+1):
@@ -14,7 +15,9 @@ for i in range(1,int(arg)+1):
     sheet.cell(row=i+1, column=1).value = i
 for rowOfCellObjects in sheet['B2':get_column_letter(int(arg)+1) + str(int(arg) + 1)]:
     for cellObj in rowOfCellObjects:
-        cellObj.value = sheet.cell(row=cellObj.row, column=1).value * sheet.cell(row=1, column=column_index_from_string(cellObj.column)).value
+        cellObj.value = sheet.cell(
+            row=cellObj.row, column=1).value * sheet.cell(
+            row=1, column=column_index_from_string(cellObj.column)).value
 boldFont = Font(bold=True)
 for cellObj in sheet.columns[0]:
     cellObj.font = boldFont
