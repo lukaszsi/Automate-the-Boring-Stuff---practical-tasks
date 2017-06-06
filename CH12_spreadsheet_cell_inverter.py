fd7e96ba8c11aee95748f91467d01be37400ada6
Lucas <inpursuitpl@gmail.com>
2017-06-06 09:38:27 +0200

    Update and rename CH12_spreadsheet_cell_inverter_before_refactoring.py to CH12_spreadsheet_cell_inverter.py

--- CH12_spreadsheet_cell_inverter.py
+++ CH12_spreadsheet_cell_inverter.py
@@ -3,11 +3,15 @@ import openpyxl
 import os
 
 from openpyxl.cell import get_column_letter, column_index_from_string
+
 wb = openpyxl.load_workbook('example.xlsx')
 sheet = wb.active
 newWb = openpyxl.Workbook()
 newSheet = newWb.active
 for tupleOfRows in sheet.rows:
     for cellObj in tupleOfRows:
-        newSheet.cell(row=column_index_from_string(cellObj.column), column=cellObj.row).value = cellObj.value
+        newSheet.cell(
+            row=column_index_from_string(cellObj.column), 
+            column=cellObj.row
+        ).value = cellObj.value
 newWb.save('inverted_example.xlsx')
