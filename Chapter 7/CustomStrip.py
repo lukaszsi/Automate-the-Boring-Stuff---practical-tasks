ad80f7e810210a8adf4b293ae84ad8ad53925797
Lucas <inpursuitpl@gmail.com>
2017-06-07 09:45:06 +0200

    Update CustomStrip.py

--- Chapter 7/CustomStrip.py
+++ Chapter 7/CustomStrip.py
@@ -2,13 +2,11 @@
 ''' Script works same as built-in strip() function. It removes
 whitespace characters or chars provided through optional argument
 from beginning and end of the given string.'''
+import re
 
-def customStrip(string, toRemove=None):
-    if toRemove== None:
-        toRemove= '\s'
+def customStrip(string, toRemove='\s'):
     wordbeginRegex = re.compile(r'^['+ toRemove+']+')
     string = wordbeginRegex.sub('', string)
     wordendRegex = re.compile(r'['+ toRemove+']+$')
     string = wordendRegex.sub('', string)
     return string
-
