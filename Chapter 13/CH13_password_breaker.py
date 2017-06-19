5da61b711350194a956fb846947afd1874b20573
Lucas <inpursuitpl@gmail.com>
2017-06-19 10:03:33 +0200

    Update and rename CH13_password_breaker_before_refactoring.py to Chapter 13/CH13_password_breaker.py

--- Chapter 13/CH13_password_breaker.py
+++ Chapter 13/CH13_password_breaker.py
@@ -7,8 +7,10 @@ os.chdir('C:\\Python')
 wordsFile = open('dictionary.txt')
 text = wordsFile.read()
 wordsList = text.split('\n')
+
 pdfFile = open('watermarkEncrypted.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFile)
+
 for word in wordsList:
     if pdfReader.decrypt(word) != 0:
         print(word)
